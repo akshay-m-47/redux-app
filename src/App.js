e6de7608546290bc7c37238341ca3472af45ed5c
Akshay Muley <akshay.mule@configserverllp.com>
2024-08-16 02:06:45 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import HomeContainer from './container/HomeContainer'
 function App() {
   return (
     <div className="App">
       <h1>Redux-App</h1>
+      <HomeContainer/>
     </div>
   );
 }
